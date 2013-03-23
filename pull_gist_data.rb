4bac186c7515402e6e4b3c7a37f911ed28ba7618
Tejus Parikh <tejus@vijedi.net>
2013-03-23 06:25:15 -0400

    parsing into the right section of the document

--- pull_gist_data.rb
+++ pull_gist_data.rb
@@ -1,4 +1,5 @@
 require 'octokit'
+require 'nokogiri'
 
 class GistEmbedder
     
@@ -10,8 +11,35 @@ class GistEmbedder
         @client = Octokit::Client.new
     end
     
-    
+    def run 
+        Dir.foreach(@postDir) do |post|
+            next if post == '.' or post == '..'
+            file = File.open(File.join(@postDir, post))
+            
+            front_matter = []
+            content = []
+            
+            append_to = front_matter
+            file.each_line do |line|
+                append_to.push(line)
+                if(line =~ /^---\s*/ and not front_matter.size == 1)
+                    append_to = content
+                end
+            end
+            
+            front_matter_str = front_matter.join('')
+            content_str = content.join('')
+            
+            fragment = Nokogiri::HTML::fragment(content_str)
+            fragment.xpath('p[@class = "embed_gist"]').each  do |node|
+                puts node
+            end
+        end
+    end
 end
+
+embedder = GistEmbedder.new(ARGV[0])
+embedder.run()
 
 # @client.gist(3880535).files.each do |file|
 #    puts file[1].content
